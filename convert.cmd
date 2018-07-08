283978dc8e2e2ecfa2d0c162da1a8309b6e2bd01
pKajan <sknitro@gmail.com>
2018-07-08 17:10:50 +0200

    initial

---
+++ convert.cmd
@@ -0,0 +1,22 @@
+SET svg_in=c:\toolbar_svg\
+SET png_out=c:\toolbar_png\
+
+inkscape.exe "%svg_in%01-play.svg" -e "%png_out%01-play.png" -d 500
+inkscape.exe "%svg_in%02-pause.svg" -e "%png_out%02-pause.png" -d 500
+inkscape.exe "%svg_in%03-stop.svg" -e "%png_out%03-stop.png" -d 500
+inkscape.exe "%svg_in%04-empty.svg" -e "%png_out%04-empty.png" -d 500
+inkscape.exe "%svg_in%05-prev.svg" -e "%png_out%05-prev.png" -d 500
+inkscape.exe "%svg_in%06-next.svg" -e "%png_out%06-next.png" -d 500
+inkscape.exe "%svg_in%07-step.svg" -e "%png_out%07-step.png" -d 500
+inkscape.exe "%svg_in%08-open.svg" -e "%png_out%08-open.png" -d 500
+inkscape.exe "%svg_in%09-empty.svg" -e "%png_out%09-empty.png" -d 500
+inkscape.exe "%svg_in%10-audio.svg" -e "%png_out%10-audio.png" -d 500
+inkscape.exe "%svg_in%11-subtitle.svg" -e "%png_out%11-subtitle.png" -d 500
+inkscape.exe "%svg_in%12-empty.svg" -e "%png_out%12-empty.png" -d 500
+inkscape.exe "%svg_in%13-sound_on.svg" -e "%png_out%13-sound_on.png" -d 500
+inkscape.exe "%svg_in%14-sound_off.svg" -e "%png_out%14-sound_off.png" -d 500
+inkscape.exe "%svg_in%15-sound.svg" -e "%png_out%15-sound.png" -d 500
+
+
+
+pause
